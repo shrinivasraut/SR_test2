d54869196bee92161919aef836cb52256db812d7
SWAPN <SWAPN@DESKTOP-K6UC9KB>
2020-12-25 19:25:59 -0800

    my 1st Commit

---
+++ src/Day1/SR_Java1.java
@@ -0,0 +1,20 @@
+package Day1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SR_Java1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium-Automation\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		System.out.println("Hello World");
+		driver.get("https://www.guru99.com/");
+		
+		driver.close();
+	}
+
+}
