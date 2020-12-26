6cb540aad4ee5c47d021db19f6c10abd89d12884
shrinivasraut <41255143+shrinivasraut@users.noreply.github.com>
2020-12-25 19:28:52 -0800

    Update SR_Java1.java

--- src/Day1/SR_Java1.java
+++ src/Day1/SR_Java1.java
@@ -11,7 +11,7 @@ public class SR_Java1 {
 		System.out.println("Hello World");
 		System.setProperty("webdriver.chrome.driver", "C:\\Selenium-Automation\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		System.out.println("Hello World");
+		System.out.println("Launching Guru99");
 		driver.get("https://www.guru99.com/");
 		
 		driver.close();
